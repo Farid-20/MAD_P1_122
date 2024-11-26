1e6af7dc44d1d51770adc04d89b76c23fd525b84
Farid <faridmandli20@gmail.com>
2024-11-26 12:55:46 +0530

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+fun main(){
+var userName = "Farid"
+    userName="Mandli"
+    var age: Int = 19
+
+    print("hello $userName your age is $age")
+}
---
+++ src/p1.12.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var a=10
+    var b=20
+    b=a+b
+    a=b-a
+
+    b=b-a
+    println("swaping without using third variable")
+    println(a)
+    println(b)
+
+
+    
+}
+
